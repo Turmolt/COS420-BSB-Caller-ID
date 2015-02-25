9a43ed8877db06c3cafb29aa7318034a058e6a3e
Chad Paradis <cparadis6191@gmail.com>
2015-02-25 15:06:55 -0500

    Got rid of the test comment

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,5 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-	// chaddev added something at the start of the comment line. Here's a test comment chadconflict added something to the end of the comment
     return a.exec();
 }
